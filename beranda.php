a4e130fa06e94c27b4d6118860ad7bf5b345e8c1
Yuvita Nur Amalia <44991732+Yuvita1211@users.noreply.github.com>
2024-07-09 19:18:59 +0700

    upload files

---
+++ beranda.php
@@ -0,0 +1,14 @@
+<div class="container-xxl">
+ <section id="hero" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+          <div>
+            <h1>Sistem Informasi Poliklinik Kita</h1>
+            <h2>Kesehatan Anda adalah prioritas kami. Bersama, kita wujudkan hidup yang lebih sehat dan bahagia.</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
